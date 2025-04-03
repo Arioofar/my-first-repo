b40c11a4e28bab7fed7d51530a20aedf27e0ab33
Arioofar <Amir.farshchin@live.com>
2025-04-02 20:20:48 -0400

    implemented functions from other project to mycode.py

--- mycode.py
+++ mycode.py
@@ -1,4 +1,8 @@
 import arcpy
 from arcpy import env
 env.workspace=r"C:\Users\afars083\Downloads\Module8_data\Module8_data\OTTAWA.gdb\ottawadata"
-print(arcpy.ListFeatureClasses())
+fclist=arcpy.ListFeatureClasses()
+
+import jj
+for fc in fclist:
+    print(jj.get_nomber(fc))
