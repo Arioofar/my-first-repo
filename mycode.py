8f1db231000d2a757735538877b548aced854860
Arioofar <Amir.farshchin@live.com>
2025-04-02 20:08:14 -0400

    added substantial changes after workspace  to mycode.py

--- mycode.py
+++ mycode.py
@@ -1,4 +1,4 @@
 import arcpy
 from arcpy import env
-env.workspace=r"C:\Users\afars083\Downloads\Module8_data\Module8_data\OTTAWA.gdb"
-arcpy.ListDatasets()
+env.workspace=r"C:\Users\afars083\Downloads\Module8_data\Module8_data\OTTAWA.gdb\ottawadata"
+print(arcpy.ListFeatureClasses())
